c8d7867c666d14b5d28beffe9e669b2c0bc1271f
absra47 <absragirma630@gmail.com>
2024-05-08 01:36:16 +0300

    taginput:autocomplete

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -3,6 +3,6 @@ import React, { useEffect } from "react";
 import { redirect } from "next/navigation";
 export default function page() {
   useEffect(() => {
-    redirect("/letters/inbox");
+    redirect("/signin");
   }, []);
 }
--- typing/index.ts
+++ typing/index.ts
@@ -1,1 +1,2 @@
 export * from "./interface/IUIState";
+export * from "./interface/IComposeState";
