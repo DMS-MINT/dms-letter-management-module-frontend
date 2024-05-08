158ed5f5f37b449f24fcc702230d9a80b8798c3c
absra47 <145018030+absra47@users.noreply.github.com>
2024-05-08 17:49:34 +0300

    Update ControlPanel.tsx

--- widgets/compose/ControlPanel.tsx
+++ widgets/compose/ControlPanel.tsx
@@ -1,5 +1,5 @@
 /** @format */
-
+"use client";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Baseline, Printer, Dot } from "lucide-react";
