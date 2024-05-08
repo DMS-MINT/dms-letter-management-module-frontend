b9eb3bcf56af6e46025b079e08fbc6ef35653bf3
absra47 <145018030+absra47@users.noreply.github.com>
2024-05-08 17:53:11 +0300

    Update OutgoingLetterForm.tsx

--- widgets/compose/composeForms/OutgoingLetterForm.tsx
+++ widgets/compose/composeForms/OutgoingLetterForm.tsx
@@ -68,7 +68,7 @@ export default function OutgoingLetterForm() {
   };
 
   return (
-    <div className="rounded-lg bg-card text-card-foreground shadow-sm p-6 h-full">
+  
       <form
         className="flex flex-col mr-4 gap-4 text-card-foreground shadow-sm p-6 h-full mb-3"
         onSubmit={(e) => e.preventDefault()}
@@ -162,6 +162,6 @@ export default function OutgoingLetterForm() {
         </Button>
         {/* <Comment comments={[]} /> */}
       </form>
-    </div>
+  
   );
 }
