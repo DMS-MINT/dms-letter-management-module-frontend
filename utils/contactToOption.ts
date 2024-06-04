b79088c13f82c986880c0e1fa30588cc3359cb3c
Yab1 <yabilisanu@gmail.com>
2024-06-04 21:58:15 +0300

    feat: Implement contactToOption function for mapping contact to options types

---
+++ utils/contactToOption.ts
@@ -0,0 +1,23 @@
+import { ContactType, IGuest, IMember, IOption, UserType } from "@/typing";
+
+const contactToOption = (contact: ContactType): IOption => {
+  if (contact.user_type === "member") {
+    const memberContact = contact as IMember;
+    return {
+      value: memberContact.id,
+      label: memberContact.job_title,
+      user_type: "member",
+    };
+  } else if (contact.user_type === "guest") {
+    const guestContact = contact as IGuest;
+    return {
+      value: guestContact.id,
+      label: guestContact.name,
+      user_type: "guest",
+    };
+  } else {
+    throw new Error("Invalid contact type");
+  }
+};
+
+export default contactToOption;
