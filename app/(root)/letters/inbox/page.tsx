5727c5eb178d73735807afbff1b04cad83b29966
Yab1 <yabilisanu@gmail.com>
2024-06-04 22:10:26 +0300

    refactor: Created a letters prop for the clerk table for dynamic data rendering

--- app/(root)/letters/inbox/page.tsx
+++ app/(root)/letters/inbox/page.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import { UserTypeEnum } from "@/typing/enum";
 import ClerkTable from "./@clerk/page";
 import StandardUserTable from "./@standard_user/page";
+import { useAppSelector } from "@/lib/hooks";
+import { selectLetters } from "@/lib/features/letter/letterSlice";
 
 export default function Page() {
+  const letters = useAppSelector(selectLetters);
   const userType: UserTypeEnum = UserTypeEnum.CLERK;
 
   return userType.valueOf() === UserTypeEnum.CLERK.valueOf() ? (
-    <ClerkTable />
+    <ClerkTable letters={letters} />
   ) : (
     <StandardUserTable />
   );
