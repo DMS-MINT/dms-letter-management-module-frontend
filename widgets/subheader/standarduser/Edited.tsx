1248e864b8a2e9d593f0d917116c121be106c5e1
Yab1 <yabilisanu@gmail.com>
2024-06-04 22:17:34 +0300

    feat: Implement letter update dispatcher in edited component

--- widgets/subheader/standarduser/Edited.tsx
+++ widgets/subheader/standarduser/Edited.tsx
@@ -2,9 +2,29 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, ChevronRight, Dot, Printer } from "lucide-react";
 import React from "react";
-import controlpanel from "../controlpanel";
 import StatusEnum from "@/typing/enum/StatusEnum";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import {
+  selectLetterDetails,
+  updateLetter,
+} from "@/lib/features/letter/letterSlice";
+import { updateLetterSerializer } from "@/utils";
+
 export default function edited() {
+  const letter = useAppSelector(selectLetterDetails);
+  const dispatch = useAppDispatch();
+
+  const dispatchLetterUpdate = () => {
+    const serializedLetter = updateLetterSerializer(letter);
+
+    dispatch(
+      updateLetter({
+        id: "68c0dfb0-26e8-41df-990d-0d554636a732",
+        letter: serializedLetter,
+      })
+    );
+  };
+
   return (
     <div className="flex items-center flex-1 ml-2 gap-2">
       <Badge
@@ -13,7 +33,6 @@ export default function edited() {
       >
         <Dot /> {StatusEnum.EDITED}
       </Badge>
-      {/* {common} */}
       <Button variant="outline" size="icon">
         <ChevronLeft size={20} />
       </Button>
@@ -23,7 +42,7 @@ export default function edited() {
       <Button variant="outline" size="icon">
         <Printer size={20} />
       </Button>
-      <Button>አርም</Button>
+      <Button onClick={dispatchLetterUpdate}>አርም</Button>
     </div>
   );
 }
