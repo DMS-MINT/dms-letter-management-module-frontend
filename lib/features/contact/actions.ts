e54240ca82232071785d7c1c0e3911b241185208
Yab1 <yabilisanu@gmail.com>
2024-06-04 22:05:03 +0300

    feat: Implement server actions for fetching user contacts

---
+++ lib/features/contact/actions.ts
@@ -0,0 +1,22 @@
+import axiosInstance from "@/lib/axiosInstance";
+
+interface ServerError {
+  message: string;
+  extra: Record<string, any>;
+}
+
+export async function get_contacts() {
+  try {
+    const response = await axiosInstance.get("users/");
+    const data = await response.data;
+    return data;
+  } catch (error: any) {
+    if (error.response && error.response.data) {
+      throw error;
+    } else if (error.request) {
+      throw new Error("Network Error: No response received");
+    } else {
+      throw new Error("Request Error: Unable to send request");
+    }
+  }
+}
