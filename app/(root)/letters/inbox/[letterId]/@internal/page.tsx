9850f5035748827ff29bc00bb6439d1116be63fe
Yab1 <yabilisanu@gmail.com>
2024-06-04 22:13:36 +0300

    fix: Resolve import error and refactor unnecessary code

--- app/(root)/letters/inbox/[letterId]/@internal/page.tsx
+++ app/(root)/letters/inbox/[letterId]/@internal/page.tsx
@@ -1,73 +1,134 @@
 "use client";
-import { Main } from "@/components/layouts";
-import TagInput from "react-tag-autocomplete";
-import { Button } from "@/components/ui/button";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Separator } from "@/components/ui/separator";
 import { Textarea } from "@/components/ui/textarea";
-import dynamic from "next/dynamic";
-import CommentSection from "@/components/layouts/Comment";
+import { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import {
+  getLetterDetails,
+  selectLetterDetails,
+  updateContent,
+  updateSubject,
+} from "@/lib/features/letter/letterSlice";
+import { contactToOption } from "@/utils";
+import { IOption, ParticipantRolesEnum } from "@/typing";
+import { selectContacts } from "@/lib/features/contact/contactSlice";
+import { SelectableInput } from "@/components/shared";
+
+interface IParticipantState {
+  role: string;
+  user: IOption;
+}
 
 export default function LetterDetail() {
-  const readOnly: boolean = true;
+  const dispatch = useAppDispatch();
+  const letter = useAppSelector(selectLetterDetails);
+  const contacts = useAppSelector(selectContacts);
+  const [options, setOptions] = useState<IOption[]>([]);
+  const [participants, setParticipants] = useState<IParticipantState[]>([]);
+
+  useEffect(() => {
+    dispatch(getLetterDetails("68c0dfb0-26e8-41df-990d-0d554636a732"));
+  }, []);
+
+  useEffect(() => {
+    if (contacts.length > 0) {
+      const options: IOption[] = contacts.map((contact) => {
+        return contactToOption(contact);
+      });
+
+      setOptions(options);
+    }
+  }, [contacts]);
+
+  useEffect(() => {
+    if (letter.participants.length > 0) {
+      const options: IParticipantState[] = [];
+
+      letter.participants.map((participant) => {
+        const option = contactToOption(participant.user);
+
+        options.push({ role: participant.role, user: option });
+      });
+
+      setParticipants(options);
+    }
+  }, [letter]);
+
+  if (participants.length === 0) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <section className="grid gap-5 h-fit pb-5 flex-1">
-      <Main>
+      <section className="card">
         <h2 className="font-semibold text-lg">የ ደብዳቤው ተሳታፊወች</h2>
-        <div className="grid-flow-row grid-cols-1 gap-5 box-border shadow-lg  p-4">
+        <div className="p-2 flex gap-2 flex-col">
           <div className="flex items-center gap-1.5">
             <Label className="w-20" htmlFor="የተቀባይ ስም">
               ከ
             </Label>
-            <Input
-              disabled={readOnly}
-              type="text"
-              id="የተቀባይ ስም"
-              value="አማረ ተፈሪ "
-              className="w-full mb-2"
+            <SelectableInput
+              defaultValue={participants
+                .filter((participant) => participant.role === "Sender")
+                .map((participant) => participant.user)}
+              options={options}
+              role={ParticipantRolesEnum.Sender}
+              isCreatable={true}
+              isMulti={true}
             />
           </div>
           <div className="flex items-center gap-1.5">
             <Label className="w-20" htmlFor="የተቀባይ ስም">
               ለ
             </Label>
-            <Input
-              disabled={readOnly}
-              type="text"
-              id="የተቀባይ ስም"
-              value="የገበያ ስራ አስኪያጅ"
-              className="w-full mb-2"
+            <SelectableInput
+              defaultValue={participants
+                .filter((participant) => participant.role === "Recipient")
+                .map((participant) => participant.user)}
+              options={options}
+              role={ParticipantRolesEnum.Recipient}
+              isCreatable={true}
+              isMulti={true}
             />
           </div>
           <div className="flex items-center gap-1.5">
             <Label className="w-20" htmlFor="ግልባጭ">
               ግልባጭ
             </Label>
-            <Input
-              disabled={readOnly}
-              type="text"
-              value="የዲጂታል ኢኮኖሚ ልማት ክፍል"
-              id="ግልባጭ"
-              className="w-full mb-2"
+            <SelectableInput
+              defaultValue={participants
+                .filter(
+                  (participant) => participant.role === "Carbon Copy Recipient"
+                )
+                .map((participant) => participant.user)}
+              options={options}
+              role={ParticipantRolesEnum["Carbon Copy Recipient"]}
+              isCreatable={true}
+              isMulti={true}
             />
           </div>
           <div className="flex items-center gap-1.5">
             <Label className="w-20" htmlFor="ግልባጭ">
               እንዲያዉቁት
             </Label>
-            <Input
-              disabled={readOnly}
-              type="text"
-              value="የዲጂታል ኢኮኖሚ ልማት ክፍል"
-              id="ግልባጭ"
-              className="w-full mb-2"
+            <SelectableInput
+              defaultValue={participants
+                .filter(
+                  (participant) =>
+                    participant.role === "Blind Carbon Copy Recipient"
+                )
+                .map((participant) => participant.user)}
+              options={options}
+              role={ParticipantRolesEnum["Blind Carbon Copy Recipient"]}
+              isCreatable={true}
+              isMulti={true}
             />
           </div>
         </div>
-      </Main>
-      <Main>
+      </section>
+      <section className="card">
         <section className="flex flex-col gap-5">
           <h2 className="font-semibold text-lg">ስለ ደብዳቤው መረጃ</h2>
           <div className="grid  gap-5">
@@ -75,20 +136,15 @@ export default function LetterDetail() {
               <div className="grid items-center gap-1.5">
                 <Label htmlFor="ጉዳዩ">ጉዳዩ</Label>
                 <Input
-                  disabled={readOnly}
                   type="text"
                   id="ጉዳዩ"
-                  value="የሩሲያ ፌዴሬሽን ባዘጋጀው የዕውቅና ሽልማት ላይ ተሳትፎ እንዲደረግ ስለማሳወቅ"
+                  value={letter.subject ? letter.subject : ""}
+                  onChange={(e) => dispatch(updateSubject(e.target.value))}
                 />
               </div>
               <div className="grid items-center gap-1.5">
                 <Label htmlFor="የገጾች ብዛት">የገጾች ብዛት</Label>
-                <Input
-                  disabled={readOnly}
-                  type="text"
-                  id="የገጾች ብዛት"
-                  value="1"
-                />
+                <Input type="text" id="የገጾች ብዛት" value="1" />
               </div>
             </div>
           </div>
@@ -97,21 +153,13 @@ export default function LetterDetail() {
 
             <Textarea
               id="ደብዳቤ"
-              disabled={readOnly}
               className="bg-gray-100 h-[500px]"
-              value="ሰላም እና ለማንኛውም ወቅታዊ ተሸላሚ ውጤት ከአማርኛ እና ከቋንቋ በላይ ተጠቃሚ ያለው ይሆናል። ስለዚህም እኔ አባልነትህን ለመስራት በእርስዎ የምንለውን ልጆች ላይ በአንድ ላይ የሚደረገውን ሰው ማስተካከል ይችላል።
-              
-              በመንግሥትና በቅናሽ በኢንተርቨት ወቅታዊ እና በምሳሌው የተደረገ መረጃ ላይ እንደሚመለከተው ይጠቀምናል። እኔም ያስችላል።
-              
-              በዚህ ትምህርት በትክክል የተደረገውን የልጆችህን እንቅስቃሴና ቅርበት እንድታደርግ በመሆኑ እኔና የአባልነትህ አባል በሆነችው ጉዳይ ላይ በድምፅ ተከትለው አንዱ እንዲያስቀምጥ ነው። ስለዚህ እናም በምንጊዜም በእኔና በአንድ ላይ የሚቀመጡ መረጃዎችን ከመዝጋት የመዘጋጀውን ማሻሻል እንችላለን።
-              
-              በመሆኑም እኔና የአባልነትህ አባል እንደሆነች የምንመለከተውን አባል በማስመልከት ከእኔ የሚሰበስበውን እንደሚረዳ እንደወዳድር እንድማለፍ በመከታተል ላይ እንደሚያስችል ይገኛል።"
+              value={letter.content ? letter.content : ""}
+              onChange={(e) => dispatch(updateContent(e.target.value))}
             />
           </section>
         </section>
-      </Main>
-      {/* <TagInput /> */}
-      {/* <CommentSection comments={[]} /> */}
+      </section>
     </section>
   );
 }
