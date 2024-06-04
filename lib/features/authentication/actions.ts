1fc130cc5cdcc55d9aa393901ec81a61f7a29325
Yab1 <yabilisanu@gmail.com>
2024-06-04 21:44:54 +0300

    feat: Implement server actions - login, logout, & fetch user profile

---
+++ lib/features/authentication/actions.ts
@@ -0,0 +1,55 @@
+import axiosInstance from "@/lib/axiosInstance";
+import { ICredentials } from "@/typing";
+
+interface ServerError {
+  message: string;
+  extra: Record<string, any>;
+}
+
+export async function get_authentication_token(credentials: ICredentials) {
+  try {
+    const response = await axiosInstance.post("auth/login/", credentials);
+    const data = await response.data;
+    return data;
+  } catch (error: any) {
+    if (error.response && error.response.data) {
+      throw error;
+    } else if (error.request) {
+      throw new Error("Network Error: No response received");
+    } else {
+      throw new Error("Request Error: Unable to send request");
+    }
+  }
+}
+
+export async function delete_authentication_token() {
+  try {
+    const response = await axiosInstance.post("auth/logout/");
+    const data = await response.data;
+    console.log(data);
+  } catch (error: any) {
+    if (error.response && error.response.data) {
+      throw error;
+    } else if (error.request) {
+      throw new Error("Network Error: No response received");
+    } else {
+      throw new Error("Request Error: Unable to send request");
+    }
+  }
+}
+
+export async function get_user_profile() {
+  try {
+    const response = await axiosInstance.get("auth/me/");
+    const data = await response.data;
+    return data;
+  } catch (error: any) {
+    if (error.response && error.response.data) {
+      throw error;
+    } else if (error.request) {
+      throw new Error("Network Error: No response received");
+    } else {
+      throw new Error("Request Error: Unable to send request");
+    }
+  }
+}
