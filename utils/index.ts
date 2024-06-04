45fffc7c25583e2a1711f2a4c691ff8ecd3574c1
Yab1 <yabilisanu@gmail.com>
2024-06-04 22:18:49 +0300

    chore: Added export dance for utility functions

--- utils/index.ts
+++ utils/index.ts
@@ -1,1 +1,5 @@
-export * from "./getRole";
+export { default as contactToOption } from "./contactToOption";
+export { default as optionToContact } from "./optionToContact";
+export { default as updateLetterSerializer } from "./updateLetterSerializer";
+export { default as getParticipantInfo } from "./getParticipantInfo";
+export { default as getTranslatedLetterStatus } from "./getTranslatedLetterStatus";
