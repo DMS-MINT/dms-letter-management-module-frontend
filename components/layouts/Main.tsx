012d715457876152194fd22795fe670dd15463e6
Yab1 <yabilisanu@gmail.com>
2024-06-06 00:49:41 +0300

    style: Added a className prop

--- components/layouts/Main.tsx
+++ components/layouts/Main.tsx
@@ -1,7 +1,9 @@
 export default function Main({
   children,
+  className,
 }: Readonly<{
   children: React.ReactNode;
+  className?: string;
 }>) {
-  return <main className="card h-fit flex-1">{children}</main>;
+  return <main className={`card h-fit flex-1 ${className}`}>{children}</main>;
 }
