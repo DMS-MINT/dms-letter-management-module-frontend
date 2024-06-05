17dd52cbfe16a08f01928657453665569c7a4e22
Yab1 <yabilisanu@gmail.com>
2024-06-06 00:26:48 +0300

    refactor: Move RootLayout component

---
+++ app/(root)/(letter management module)/letters/[category]/[letterId]/layout.tsx
@@ -0,0 +1,25 @@
+import {
+  DetailControlPanel,
+  LetterDetailsDrawer,
+} from "@/components/features/letter";
+import { Subheader, Drawer } from "@/components/layouts";
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <>
+      <Subheader>
+        <section className="flex items-center justify-between w-full">
+          <DetailControlPanel />
+        </section>
+      </Subheader>
+      <section className="flex px-8 gap-6 mt-2 h-full">
+        <Drawer>
+          <LetterDetailsDrawer />
+        </Drawer>
+        {children}
+      </section>
+    </>
+  );
+}
