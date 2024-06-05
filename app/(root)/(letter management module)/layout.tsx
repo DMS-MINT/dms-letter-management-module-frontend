817e375257884e4d3a385b64014132cffd4f0cd0
Yab1 <yabilisanu@gmail.com>
2024-06-06 00:22:53 +0300

    refactor: Move RootLayout component

---
+++ app/(root)/(letter management module)/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Noto_Serif_Ethiopic } from "next/font/google";
+import "@/app/globals.css";
+import { StoreProvider } from "@/app/StoreProvider";
+import { TopBar } from "@/components/layouts";
+import { Toaster } from "sonner";
+
+const noto_serif_ethiopic = Noto_Serif_Ethiopic({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Letter Management System",
+  description: "Efficiently manage and organize your letters.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <StoreProvider>
+      <html lang="am">
+        <body className={noto_serif_ethiopic.className}>
+          <div className="absolute">
+            <Toaster richColors position="top-center" />
+          </div>
+          <TopBar />
+          {children}
+        </body>
+      </html>
+    </StoreProvider>
+  );
+}
