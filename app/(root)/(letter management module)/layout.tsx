e733fac388f38b825d07a381e9f41950b88950b1
Yab1 <yabilisanu@gmail.com>
2024-06-06 12:56:17 +0300

    feat: Wrap app content with RequireAuth component for authentication checks

--- app/(root)/(letter management module)/layout.tsx
+++ app/(root)/(letter management module)/layout.tsx
@@ -4,6 +4,7 @@ import "@/app/globals.css";
 import { StoreProvider } from "@/app/StoreProvider";
 import { TopBar } from "@/components/layouts";
 import { Toaster } from "sonner";
+import { RequireAuth } from "@/components/utils";
 
 const noto_serif_ethiopic = Noto_Serif_Ethiopic({ subsets: ["latin"] });
 
@@ -21,11 +22,13 @@ export default function RootLayout({
     <StoreProvider>
       <html lang="am">
         <body className={noto_serif_ethiopic.className}>
-          <div className="absolute">
-            <Toaster richColors position="top-center" />
-          </div>
-          <TopBar />
-          {children}
+          <RequireAuth>
+            <div className="absolute">
+              <Toaster richColors position="top-center" />
+            </div>
+            <TopBar />
+            {children}
+          </RequireAuth>
         </body>
       </html>
     </StoreProvider>
