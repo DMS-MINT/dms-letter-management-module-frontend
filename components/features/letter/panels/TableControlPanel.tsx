444e79437a9198fcdc799e3a5f95b5633af09d0d
Yab1 <yabilisanu@gmail.com>
2024-06-09 16:52:15 +0300

    refactor: Implement category validation in dynamic route for letters

--- components/features/letter/panels/TableControlPanel.tsx
+++ components/features/letter/panels/TableControlPanel.tsx
@@ -1,17 +1,34 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { letterCategoryLookup } from "@/typing/dictionary";
 import { Plus, RotateCw } from "lucide-react";
 import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const whitelist: string[] = ["inbox", "outbox", "draft", "archive", "trash"];
 
 export default function TableControlPanel() {
+  const params = useParams();
+  const router = useRouter();
+  const category: string = params.category as string;
+
+  useEffect(() => {
+    if (!whitelist.includes(params.category as string)) {
+      router.push("/404");
+    }
+  }, [params.category]);
+
   function refreshPage() {
     window.location.reload();
   }
 
   return (
     <section className="flex items-center justify-between w-full">
-      <h1 className="page-title">ገቢ ደብዳቤዎች</h1>
+      <h1 className="page-title">
+        {letterCategoryLookup[category.toUpperCase()]}
+      </h1>
       <div className="flex items-center gap-4">
         <Button variant="outline" size="icon" onClick={refreshPage}>
           <RotateCw className="w-5 h-5" />
