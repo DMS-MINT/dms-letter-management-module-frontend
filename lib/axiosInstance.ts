e0073a331c917ca52dd7192f34265d87c8703ea9
Yab1 <yabilisanu@gmail.com>
2024-06-09 12:37:45 +0300

    feat: Add request interceptor to include session ID in requests

--- lib/axiosInstance.ts
+++ lib/axiosInstance.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance } from "axios";
+import { get_session } from "./features/authentication/actions";
 
 axios.defaults.withCredentials = true;
 
@@ -9,5 +10,23 @@ const axiosInstance: AxiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// Add a request interceptor to include the session ID in every request
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    if (!config.url?.includes("auth/login/")) {
+      const session = await get_session();
+      const sessionId = session.sessionId;
+
+      if (sessionId) {
+        config.headers.Authorization = `Session ${sessionId}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
