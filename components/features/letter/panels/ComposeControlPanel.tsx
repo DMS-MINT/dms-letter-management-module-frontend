f8787cd12c24fffa24e0da5fde3ec02b601e83fd
absra47 <145018030+absra47@users.noreply.github.com>
2024-06-17 14:56:10 +0300

    fix-compose and draft (#11)

--- components/features/letter/panels/ComposeControlPanel.tsx
+++ components/features/letter/panels/ComposeControlPanel.tsx
@@ -50,39 +50,37 @@ export default function ComposeControlPanel() {
   }
 
   return (
-    <section className='flex items-center justify-between w-full overflow-auto'>
-      <div className='flex gap-2 no-print'>
-        <h1 className='page-title'>አዲስ ደብዳቤ </h1>
+    <section className="flex items-center justify-between w-full overflow-auto">
+      <div className="flex gap-2 no-print">
+        <h1 className="page-title">አዲስ ደብዳቤ </h1>
         <Badge
-          variant='destructive'
-          className='rounded-md flex items-center justify-between pl-0 '
+          variant="destructive"
+          className="rounded-md flex items-center justify-between pl-0 "
         >
           <Dot /> አልተቀመጠም
         </Badge>
       </div>
-      <div className='flex items-center gap-3 no-print'>
-        <Link href='/letters/${category}/${letterDetail.id}/print'>
-          <Button variant='outline' size='icon'>
+      <div className="flex items-center gap-3 no-print">
+        <Link href="/letters/${category}/${letterDetail.id}/print">
+          <Button variant="outline" size="icon">
             <Printer size={20} />
           </Button>
         </Link>
         <Button
-          className='mr-0 RECIPIENTborder-gray-300 rounded-md'
-          variant='outline'
-          onClick={() => {
-            dispatchSetLetterStatus("Draft");
-          }}
+          className="mr-0 RECIPIENTborder-gray-300 rounded-md"
+          variant="outline"
+          onClick={dispatchCreateLetter}
         >
           ረቂቁን ያስቀምጡ
         </Button>
 
-        <Button className='ml-0 border-gray-300 rounded-md' variant='outline'>
+        <Button className="ml-0 border-gray-300 rounded-md" variant="outline">
           ደብዳቤውን ምራ
         </Button>
 
         <Dialog>
           <DialogTrigger asChild>
-            <Button variant='default'>ላክ</Button>
+            <Button variant="default">ላክ</Button>
           </DialogTrigger>
           <DialogContent>
             <DialogHeader>
@@ -92,13 +90,10 @@ export default function ComposeControlPanel() {
               </DialogDescription>
             </DialogHeader>
             <DialogFooter>
-              <Button
-                type='submit'
-                onClick={() => dispatchSetLetterStatus("Pending Approval")}
-              >
+              <Button type="submit" onClick={dispatchCreateLetter}>
                 አዎ
               </Button>
-              <Button className='bg-white text-black hover:bg-white'>አይ</Button>
+              <Button className="bg-white text-black hover:bg-white">አይ</Button>
             </DialogFooter>
           </DialogContent>
         </Dialog>
