7d72bd399b82cd5b2a7a00605cd4d6a9b921693c
Yab1 <yabilisanu@gmail.com>
2024-06-30 10:58:01 +0300

    fix: fixed critical bugs causing application crashes during data processing.

--- lib/features/letter/letterSlice.ts
+++ lib/features/letter/letterSlice.ts
@@ -93,7 +93,6 @@ export const letterSlice = createAppSlice({
       }
     ),
     signLetter: create.reducer((state, action: PayloadAction<File>) => {
-      console.log(action.payload);
       state.letterDetails.signature = action.payload;
     }),
     removeSignature: create.reducer((state, _) => {
