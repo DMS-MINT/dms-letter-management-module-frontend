c98dfe2380b500bb1886848374e0c253f6c6b2de
Yab1 <yabilisanu@gmail.com>
2024-07-25 17:15:45 +0300

    refactor(date): update import method for Ethiopian date conversion
    - Changed the import method for the `ethiopian-date` module from ES module import to CommonJS require.
    - Updated the usage of `toEthiopian` function to align with the new import method.
    
    This refactor ensures compatibility with the CommonJS module system.

--- utils/convertToEthiopianDate.ts
+++ utils/convertToEthiopianDate.ts
@@ -1,10 +1,10 @@
 import { monthTranslations } from "@/types/letter_module";
-import { toEthiopian } from "ethiopian-date";
+const ethiopianDate = require("ethiopian-date");
 
 export default function convertToEthiopianDate(dateString: string): string {
 	const date = new Date(dateString);
 
-	const [ethYear, ethMonth, ethDay] = toEthiopian(
+	const [ethYear, ethMonth, ethDay] = ethiopianDate.toEthiopian(
 		date.getFullYear(),
 		date.getMonth() + 1,
 		date.getDate()
