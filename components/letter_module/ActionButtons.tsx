4e15c8835532740d51da5c1311981a8dc0106adf
Yab1 <yabilisanu@gmail.com>
2024-07-25 12:28:36 +0300

    feat: Refactor ActionButtons component for enhanced functionality and performance
    - Replaced `getActionButtonConfigs` with `generateUserPermissions` to handle button configurations based on user permissions.
    - Added support for `ActionConfirmModal` and various dialog components (`PermanentlyDeleteDialog`, `ShareLetterDialog`, `SubmitLetterDialog`) for interactive actions.
    - Switched to `useMemo` for optimizing button configurations calculation.
    - Introduced `modelRef` to handle OTP input for confirmation modals.
    - Updated button rendering to use `id` for unique keys instead of `uuidv4()`.
    - Added new button actions for `trash_letter`, `restore_letter`, `permanently_delete`, `retract_letter`, `reject_letter`, `publish_letter`, `close_letter`, and `reopen_letter`.
    - Used `memo` to optimize component rendering.
    - Adjusted component visibility based on permissions and letter type.

--- components/letter_module/ActionButtons.tsx
+++ components/letter_module/ActionButtons.tsx
@@ -1,39 +1,191 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { getActionButtonConfigs } from "@/utils";
-import React, { useEffect, useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import { generateUserPermissions } from "@/utils";
+import React, { memo, useMemo, useRef } from "react";
 import { LetterDetailResponseType } from "@/types/letter_module";
 import { useWorkflowDispatcher } from "@/hooks";
-import type { PropType } from "@/hooks";
-import type { ButtonConfigType } from "@/utils";
+import {
+	PermanentlyDeleteDialog,
+	ShareLetterDialog,
+	SubmitLetterDialog,
+} from "@/components/letter_module";
+import { Trash } from "lucide-react";
+import { v4 as uuidv4 } from "uuid";
+import type { ActionType, PropType } from "@/hooks";
+import { ActionConfirmModal } from "../shared";
+import { ActionConfirmModalRef } from "../shared/ActionConfirmModal";
 
-export default function ActionButtons({
-	data,
+export type ButtonConfigType = {
+	id: string;
+	isVisible: boolean;
+	component?: JSX.Element;
+	label?: string | null;
+	icon?: JSX.Element;
+	variant?: "default" | "destructive" | "outline" | "third";
+	size?: "default" | "icon";
+	style?: string;
+	action?: () => void;
+};
+
+function ActionButtons({
+	data: { letter, permissions },
 }: {
 	data: LetterDetailResponseType;
 }) {
+	const modelRef = useRef<ActionConfirmModalRef>(null);
 	const { mutate } = useWorkflowDispatcher();
-	const [buttonConfigs, setButtonConfigs] = useState<ButtonConfigType[]>([]);
 
-	const handleClick = (action: PropType) => {
-		mutate(action);
+	const handleAction = (actionType: ActionType, otp?: number) => {
+		mutate({
+			actionType,
+			params: { referenceNumber: letter.reference_number, otp: otp },
+		});
 	};
 
-	useEffect(() => {
-		const configs = getActionButtonConfigs(data);
-		setButtonConfigs(configs);
-	}, [data]);
+	const buttonConfigs: ButtonConfigType[] = useMemo(() => {
+		const currentUserPerms = generateUserPermissions(permissions);
+
+		return [
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_trash_letter,
+				variant: "outline",
+				size: "icon",
+				icon: <Trash size={20} />,
+				action: () => handleAction("trash_letter"),
+			},
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_share_letter,
+				component: <ShareLetterDialog letter={letter} />,
+			},
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_restore_letter,
+				label: "ወደነበረበት መልስ",
+				variant: "default",
+				size: "default",
+				action: () => handleAction("restore_letter"),
+			},
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_permanently_delete_letter,
+				component: (
+					<ActionConfirmModal
+						ref={modelRef}
+						triggerButtonText="በቋሚነት ያስወግዱ"
+						triggerButtonVariant="destructive"
+						dialogTitle="ደብዳቤውን በቋሚነት አጥፋ"
+						dialogDescription="እርግጠኛ ነዎት ይህን ደብዳቤ እስከመጨረሻው መሰረዝ ይፈልጋሉ? እባክዎ ለመቀጠል ውሳኔዎን ያረጋግጡ።"
+						cancelButtonText="አይ"
+						confirmButtonText="አዎ"
+						onConfirm={() => {
+							const otp: number | undefined = modelRef.current?.getOTP();
+							if (!otp) return;
+							handleAction("permanently_delete", otp);
+						}}
+						requiresAuth={true}
+					/>
+				),
+			},
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_submit_letter,
+				component: <SubmitLetterDialog letter={letter} />,
+			},
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_retract_letter,
+				component: (
+					<ActionConfirmModal
+						ref={modelRef}
+						triggerButtonText="ለክለሳ መልስ"
+						triggerButtonVariant="destructive"
+						dialogTitle="ለክለሳ ደብዳቤ መልስ"
+						dialogDescription="እርግጠኛ ነዎት ይህን ደብዳቤ ለክለሳ መመለስ ይፈልጋሉ? ይህ እርምጃ ደብዳቤውን ከታተመ ዝርዝር ውስጥ ያስወግዳል, ይህም አስፈላጊ ለውጦችን እንዲያደርጉ ያስችልዎታል. እባክዎ ለመቀጠል ውሳኔዎን ያረጋግጡ።"
+						cancelButtonText="አይ"
+						confirmButtonText="አዎ"
+						onConfirm={() => {
+							const otp: number | undefined = modelRef.current?.getOTP();
+							if (!otp) return;
+							handleAction("retract_letter", otp);
+						}}
+						requiresAuth={true}
+					/>
+				),
+			},
+			{
+				id: uuidv4(),
+				isVisible:
+					letter.letter_type !== "incoming" && currentUserPerms.can_reject_letter,
+				component: (
+					<ActionConfirmModal
+						ref={modelRef}
+						triggerButtonText="ደብዳቤውን አትቀበል"
+						triggerButtonVariant="destructive"
+						dialogTitle="ደብዳቤውን አትቀበል"
+						dialogDescription="እርግጠኛ ነዎት ይህን ደብዳቤ አለመቀበል ይፈልጋሉ? እባክዎ ለመቀጠል ውሳኔዎን ያረጋግጡ።"
+						cancelButtonText="አይ"
+						confirmButtonText="አዎ"
+						onConfirm={() => {
+							const otp: number | undefined = modelRef.current?.getOTP();
+							if (!otp) return;
+							handleAction("reject_letter", otp);
+						}}
+						requiresAuth={true}
+					/>
+				),
+			},
+			{
+				id: uuidv4(),
+				isVisible:
+					letter.letter_type !== "incoming" && currentUserPerms.can_publish_letter,
+				component: (
+					<ActionConfirmModal
+						ref={modelRef}
+						triggerButtonText="ደብዳቤውን አከፋፍል"
+						triggerButtonVariant="third"
+						dialogTitle="ደብዳቤውን አከፋፍል"
+						dialogDescription="እርግጠኛ ነዎት ይህን ደብዳቤ ማከፋፈል ይፈልጋሉ? እባክዎ ለመቀጠል ውሳኔዎን ያረጋግጡ።"
+						cancelButtonText="አይ"
+						confirmButtonText="አዎ"
+						onConfirm={() => {
+							const otp: number | undefined = modelRef.current?.getOTP();
+							if (!otp) return;
+							handleAction("publish_letter", otp);
+						}}
+						requiresAuth={true}
+					/>
+				),
+			},
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_close_letter,
+				label: "የደብዳቤውን የስራ ሂደት አጠናቅ",
+				variant: "third",
+				size: "default",
+				action: () => handleAction("close_letter"),
+			},
+			{
+				id: uuidv4(),
+				isVisible: currentUserPerms.can_reopen_letter,
+				label: "የደብዳቤውን የስራ ሂደት እንደገና ክፈት",
+				variant: "default",
+				size: "default",
+				action: () => handleAction("reopen_letter"),
+			},
+		];
+	}, [letter, permissions]);
 
 	return (
 		<>
 			{buttonConfigs
 				.filter((action) => action.isVisible === true)
-				.map(({ label, icon, variant, size, style, action, isButton, component }) =>
-					isButton ? (
+				.map(({ id, label, icon, variant, size, style, action, component }) =>
+					!component ? (
 						<Button
-							key={uuidv4()}
+							key={id}
 							variant={variant}
 							size={size}
 							className={style}
@@ -43,9 +195,11 @@ export default function ActionButtons({
 							{icon}
 						</Button>
 					) : (
-						<React.Fragment key={uuidv4()}>{component}</React.Fragment>
+						<React.Fragment key={id}>{component}</React.Fragment>
 					)
 				)}
 		</>
 	);
 }
+
+export default memo(ActionButtons);
