7f86e2bcde51b7c03baf353a1e47f5719da82fd0
Yab1 <yabilisanu@gmail.com>
2024-07-25 12:47:12 +0300

    style(detail-control-panel): confirm no changes

--- components/letter_module/DetailControlPanel.tsx
+++ components/letter_module/DetailControlPanel.tsx
@@ -1,43 +1,18 @@
-"use client";
-import { useEffect, useState } from "react";
 import { LetterDetailResponseType } from "@/types/letter_module";
-import { SubmitLetterDialog, ActionButtons } from ".";
+import { ActionButtons, StatusBadge } from ".";
 
-interface IContentJson {
-	content: string;
-}
-
-export default function DetailControlPanel<T>({
+export default function DetailControlPanel({
 	data,
 }: {
 	data: LetterDetailResponseType;
 }) {
-	const [contentJson, setContentJson] = useState<IContentJson[]>([]);
-	const [myPerms, setMyPerms] = useState<Array<string>>([]);
-
-	useEffect(() => {
-		const my_perms = data.permissions.filter(
-			(perms) => perms.is_current_user === true
-		)[0].permissions;
-
-		setMyPerms(my_perms);
-	}, [data]);
-
-	// console.log(my_perms.find((perm) => perm === "can_update_letter"));
-	// console.log(my_perms.find((perm) => perm === "can_update_letterr"));
-	// useEffect(() => {
-	// 	setContentJson([
-	// 		{ content: letterDetails?.content ? letterDetails?.content : "" },
-	// 	]);
-	// }, [letterDetails]);
-
 	return (
 		<section className="flex items-center justify-between w-full">
-			letter.subject ? (
-			<h1 className="page-title limited-chars ">{data.letter.subject}</h1>) : (
-			<h1 className="page-title !text-gray-400">ርዕሰ ጉዳይ የሌለው ደብዳቤ</h1>)
+			<h1 className="page-title limited-chars ">
+				{data.letter.subject ? data.letter.subject : "ርዕሰ ጉዳይ የሌለው ደብዳቤ"}
+			</h1>
+			<StatusBadge current_state={data.letter.current_state} />
 			<div className="flex items-center ml-auto gap-2">
-				<SubmitLetterDialog />
 				{/* <PrintPreviewButton /> */}
 				<ActionButtons data={data} />
 			</div>
