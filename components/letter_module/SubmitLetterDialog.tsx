f1f679344b166691baa789d2c655760ff6982b49
Yab1 <yabilisanu@gmail.com>
2024-07-25 12:44:19 +0300

    feat(dialog): add OTP verification and default signature functionality
    Added functionality to the `SubmitLetterDialog` component to handle OTP verification and display the default signature.
    
    - Integrated `useOTP` hook for managing OTP input and changes.
    - Used `useMutation` from `react-query` to fetch the default signature and manage loading and error states.
    - Updated dialog content to show OTP input form when the default signature is not yet available and display the signature image upon successful verification.
    - Enhanced the dialog with conditional rendering based on the success or pending state of the signature fetch operation.
    
    This update improves the user experience by providing a clear flow for OTP verification

--- components/letter_module/SubmitLetterDialog.tsx
+++ components/letter_module/SubmitLetterDialog.tsx
@@ -9,57 +9,29 @@ import {
 	DialogTitle,
 	DialogTrigger,
 	DialogFooter,
+	DialogClose,
 } from "@/components/ui/dialog";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "@/components/ui/form";
-import {
-	InputOTP,
-	InputOTPGroup,
-	InputOTPSlot,
-} from "@/components/ui/input-otp";
 import Link from "next/link";
 import { LINKS } from "@/constants";
 import { getDefaultSignature } from "@/actions/signature_module/action";
 import { useEffect } from "react";
+import { useOTP, useWorkflowDispatcher } from "@/hooks";
+import { OTPInputForm } from "../shared";
+import { LetterDetailType } from "@/types/letter_module";
 
-const formSchema = z.object({
-	otp: z
-		.number()
-		.int()
-		.refine(
-			(value) => {
-				const strValue = value.toString();
-				return strValue.length === 6 && /^[0-9]+$/.test(strValue);
-			},
-			{
-				message: "የአንድ ጊዜ የይለፍ ቃል ባለ 6-አሃዝ መሆን አለበት።",
-			}
-		),
-});
+export default function SubmitLetterDialog({
+	letter,
+}: {
+	letter: LetterDetailType;
+}) {
+	const { form, getOTP, handleInputChange } = useOTP();
+	const { mutate, isPending, isSuccess } = useWorkflowDispatcher();
 
-export default function SubmitLetter() {
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-	});
-
-	const {
-		mutate: getDefaultSignatureMutate,
-		data: e_signature,
-		isPending,
-	} = useMutation({
+	const getDefaultSignatureMutation = useMutation({
 		mutationKey: ["getDefaultSignature"],
-		mutationFn: (otp: number) => getDefaultSignature(otp),
+		mutationFn: getDefaultSignature,
 		onMutate: () => {
 			toast.dismiss();
 			toast.loading("የእርስዎን የማረጋገጫ ኮድ በማረጋገጥ ላይ። እባክዎ ይጠብቁ...");
@@ -73,20 +45,6 @@ export default function SubmitLetter() {
 			toast.error(error.message);
 		},
 	});
-
-	useEffect(() => {
-		console.log(e_signature);
-	}, [e_signature]);
-
-	const oneTimePassword = form.watch("otp");
-
-	const handleInputChange = (value: string) => {
-		form.setValue("otp", Number(value));
-	};
-
-	function onSubmit(values: z.infer<typeof formSchema>) {
-		getDefaultSignatureMutate(values.otp);
-	}
 
 	return (
 		<Dialog>
@@ -102,63 +60,81 @@ export default function SubmitLetter() {
 			</DialogTrigger>
 			<DialogContent className="">
 				<DialogHeader className="gap-2">
-					<DialogTitle>ማንነቶን ያረጋግጡ</DialogTitle>
-					<DialogDescription>
-						በ
-						<Link
-							href={LINKS.google_authenticator}
-							className="text-blue-800"
-							target="_blank"
-						>
-							{" Google Authenticator "}
-						</Link>
-						መተግበሪያዎ ላይ ያለውን ባለ 6-አሃዝ የማረጋገጫ ኮድ ያስገቡ።
-					</DialogDescription>
-					{e_signature ? (
+					{getDefaultSignatureMutation.isSuccess ? (
+						<DialogTitle>ወደ መዝገብ ቢሮ አስተላልፍ</DialogTitle>
+					) : (
+						<DialogTitle>ማንነትዎን ያረጋግጡ</DialogTitle>
+					)}
+					{getDefaultSignatureMutation.isSuccess ? (
+						<DialogDescription>
+							እርግጠኛ ኖት ደብዳቤውን ወደ መዝገብ ቢሮ ማስገባት ይፈልጋሉ? እባክዎ ለመቀጠል ውሳኔዎን ያረጋግጡ።
+						</DialogDescription>
+					) : (
+						<DialogDescription>
+							ይህን ደብዳቤ ደህንነቱ በተጠበቀ መልኩ ለመፈረም፣ እባክዎ በእርስዎ
+							{
+								<Link
+									href={LINKS.google_authenticator}
+									className="text-blue-800"
+									target="_blank"
+								>
+									{" Google Authenticator "}
+								</Link>
+							}
+							መተግበሪያ የመነጨውን የአንድ ጊዜ የይለፍ ቃል ያስገቡ።
+						</DialogDescription>
+					)}
+				</DialogHeader>
+				<div className="px-2">
+					{getDefaultSignatureMutation.isSuccess ? (
 						<div className="flex justify-center h-60">
 							<img
-								src={`${process.env.NEXT_PUBLIC_BASE_API_URL}${e_signature}`}
-								alt="Signature"
+								src={`${process.env.NEXT_PUBLIC_BASE_API_URL}${getDefaultSignatureMutation.data.e_signature}`}
+								alt="Your Signature"
 							/>
 						</div>
-					) : null}
-					<Form {...form}>
-						<form
-							onSubmit={(e) => {
-								e.preventDefault();
+					) : (
+						<OTPInputForm form={form} onChange={handleInputChange} />
+					)}
+				</div>
+				<DialogFooter>
+					<DialogClose asChild>
+						{getDefaultSignatureMutation.isSuccess ? (
+							<Button variant="outline" onClick={getDefaultSignatureMutation.reset}>
+								አይ
+							</Button>
+						) : (
+							<Button variant="outline">ሰርዝ</Button>
+						)}
+					</DialogClose>
+					{getDefaultSignatureMutation.isSuccess ? (
+						<Button
+							disabled={isPending}
+							type="button"
+							onClick={() => {
+								mutate({
+									actionType: "submit_letter",
+									params: { referenceNumber: letter.reference_number },
+								});
 							}}
 						>
-							<FormField
-								control={form.control}
-								name="otp"
-								render={() => (
-									<FormItem>
-										<FormControl>
-											<InputOTP maxLength={6} onChange={handleInputChange}>
-												<InputOTPGroup className="w-full">
-													{Array.from({ length: 6 }).map((_, index) => (
-														<InputOTPSlot key={index} index={index} className="flex-1" />
-													))}
-												</InputOTPGroup>
-											</InputOTP>
-										</FormControl>
-										<FormMessage className="form-error-message" />
-									</FormItem>
-								)}
-							/>
-						</form>
-					</Form>
-				</DialogHeader>
-				<DialogFooter>
-					<Button
-						disabled={
-							!oneTimePassword || oneTimePassword.toString().length !== 6 || isPending
-						}
-						type="submit"
-						onClick={form.handleSubmit(onSubmit)}
-					>
-						ቀጥል
-					</Button>
+							አዎ
+						</Button>
+					) : (
+						<Button
+							disabled={
+								getOTP()?.toString().length !== 6 ||
+								getDefaultSignatureMutation.isPending
+							}
+							type="submit"
+							onClick={() => {
+								const otp = getOTP();
+								getDefaultSignatureMutation.mutate(otp);
+							}}
+						>
+							ቀጥል
+						</Button>
+					)}
 				</DialogFooter>
 			</DialogContent>
 		</Dialog>
