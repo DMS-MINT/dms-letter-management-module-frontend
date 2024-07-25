ca6e325eac71b875129f290df21c26ede5d8f298
Yab1 <yabilisanu@gmail.com>
2024-07-25 12:35:04 +0300

    refactor(TableControlPanel): optimize category validation and translations
    Updated the `TableControlPanel` component to use `letterCategoryTranslations` instead of `letterCategoryLookup`. Introduced `useMemo` for optimized category validation and title retrieval. Improved the `useEffect` dependency array for better performance and maintainability.

--- components/letter_module/TableControlPanel.tsx
+++ components/letter_module/TableControlPanel.tsx
@@ -1,49 +1,53 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { letterCategoryLookup } from "@/typing/dictionary";
+import { letterCategoryTranslations } from "@/types/letter_module";
 import { Plus } from "lucide-react";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 const whitelist: string[] = [
-  "inbox",
-  "outbox",
-  "draft",
-  "pending",
-  "published",
-  "trash",
+	"inbox",
+	"outbox",
+	"draft",
+	"pending",
+	"published",
+	"trash",
 ];
 
 export default function TableControlPanel() {
-  const params = useParams();
-  const router = useRouter();
-  const category: string = params.category as string;
+	const params = useParams();
+	const router = useRouter();
+	const category: string = params.category as string;
 
-  useEffect(() => {
-    if (!whitelist.includes(params.category as string)) {
-      router.push("/404");
-    }
-  }, [params.category]);
+	const isValidCategory = useMemo(
+		() => whitelist.includes(category),
+		[category]
+	);
 
-  function refreshPage() {
-    window.location.reload();
-  }
+	const categoryTitle = useMemo(
+		() => letterCategoryTranslations[category.toUpperCase()],
+		[category]
+	);
 
-  return (
-    <section className="flex items-center justify-between w-full">
-      <h1 className="page-title">
-        {letterCategoryLookup[category.toUpperCase()]}
-      </h1>
-      <div className="flex items-center gap-4">
-        <Link href="/letters/compose">
-          <Button className="flex gap-1 w-fit items-center">
-            <Plus size={19} />
-            አዲስ ደብዳቤ
-          </Button>
-        </Link>
-      </div>
-    </section>
-  );
+	useEffect(() => {
+		if (!isValidCategory) {
+			router.push("/404");
+		}
+	}, [isValidCategory, router]);
+
+	return (
+		<section className="flex items-center justify-between w-full">
+			<h1 className="page-title">{categoryTitle}</h1>
+			<div className="flex items-center gap-4">
+				<Link href="/letters/compose">
+					<Button className="flex gap-1 w-fit items-center">
+						<Plus size={19} />
+						አዲስ ደብዳቤ
+					</Button>
+				</Link>
+			</div>
+		</section>
+	);
 }
