d3b8cd03f2bfb85b87b0900b0ce9902ea831d566
Yab1 <yabilisanu@gmail.com>
2024-07-25 12:27:42 +0300

    refactor: Update letter detail page components and layout
    - Replaced `ActionButtons` with `DetailControlPanel` in `Subheader`.
    - Updated toast loading message to correct translation for letter details.
    - Adjusted import path for `DetailControlPanel` in `letter_module`.

--- app/(root)/(letter management module)/letters/[category]/[referenceNumber]/page.tsx
+++ app/(root)/(letter management module)/letters/[category]/[referenceNumber]/page.tsx
@@ -9,7 +9,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { toast } from "sonner";
 import {
-	ActionButtons,
+	DetailControlPanel,
 	LetterDetailsDrawer,
 	LetterSkeleton,
 } from "@/components/letter_module";
@@ -22,7 +22,7 @@ export default function LetterDetail() {
 		queryFn: async () => {
 			try {
 				toast.dismiss();
-				toast.loading("የደብዳቤ ዝርዝር መረጃ በማምጣት ላይ፣ እባክዎ ይጠብቁ...");
+				toast.loading("የደብዳቤዉን ዝርዝር መረጃ በማምጣት ላይ፣ እባክዎ ይጠብቁ...");
 				const data: LetterDetailResponseType = await getLetterDetails(
 					referenceNumber as string
 				);
@@ -39,16 +39,7 @@ export default function LetterDetail() {
 	return isSuccess && data ? (
 		<main className="flex flex-col h-full">
 			<Subheader>
-				<section className="flex items-center justify-between w-full">
-					{data.letter.subject ? (
-						<h1 className="page-title limited-chars ">{data.letter.subject}</h1>
-					) : (
-						<h1 className="page-title !text-gray-400">ርዕሰ ጉዳይ የሌለው ደብዳቤ</h1>
-					)}
-					<div className="flex items-center ml-auto gap-2">
-						<ActionButtons data={data} />
-					</div>
-				</section>
+				<DetailControlPanel data={data} />
 			</Subheader>
 			<section className="flex px-5 gap-6 mt-2 flex-1">
 				<Drawer>
