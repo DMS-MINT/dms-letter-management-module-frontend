cc2f14a27ea7015dfddbb8fc2b41c1dee981f32b
Yab1 <yabilisanu@gmail.com>
2024-07-26 20:31:39 +0300

    feat(compose): integrate React Query for creating letters
    - Replaced commented-out code with a new implementation using React Query.
    - Added useMutation hook to handle letter creation with loading, success, and error states.
    - Integrated toast notifications for user feedback during the letter creation process.
    - Used useAppSelector to fetch new letter details from the Redux store.
    - Implemented navigation to the draft letter page upon successful creation.

--- components/letter_module/ComposeControlPanel.tsx
+++ components/letter_module/ComposeControlPanel.tsx
@@ -1,56 +1,58 @@
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { Button } from "../ui/button";
+import { createLetter } from "@/actions/letter_module/crudActions";
+import { toast } from "sonner";
+import { useAppSelector } from "@/hooks";
+import { selectNewLetter } from "@/lib/features/letterSlice";
+import { LetterDetailType } from "@/types/letter_module";
+import { useRouter } from "next/navigation";
+
 export default function ComposeControlPanel() {
-	// useEffect(() => {
-	// 	dispatch(toggleDrawerVisibility(true));
-	// }, []);
+	const newLetter = useAppSelector(selectNewLetter);
+	const router = useRouter();
+	const { mutate } = useMutation({
+		mutationKey: ["createLetter"],
+		mutationFn: async () => {
+			const response = await createLetter(newLetter);
 
-	// useEffect(() => {
-	// 	setContentJson([
-	// 		{ content: letterDetails?.content ? letterDetails?.content : "" },
-	// 	]);
-	// }, [letterDetails]);
+			if (!response.ok) throw response;
 
-	// const dispatchCreateLetter = () => {
-	// 	if (letterDetails) {
-	// 		const composeFormData = letterSerializer(
-	// 			letterDetails,
-	// 			attachments,
-	// 			letterDetails.signature
-	// 		);
-	// 		dispatch(createLetter(composeFormData));
-	// 	}
-	// };
+			return response.message as LetterDetailType;
+		},
+		onMutate: () => {
+			toast.dismiss();
+			toast.loading("ደብዳቤ በመፍጠር ላይ፣ እባክዎ ይጠብቁ...");
+		},
+		onSuccess: (data) => {
+			toast.dismiss();
+			toast.success("ደብዳቤ በተሳካ ሁኔታ ተፈጥሯል!");
+			router.push(`/letters/draft/${data.reference_number}`);
+			console.log(data);
+		},
+		onError: (error: any) => {
+			toast.dismiss();
+			toast.error(error.message);
+		},
+	});
 
-	// useEffect(() => {
-	// 	if (
-	// 		status === RequestStatusEnum.FULFILLED &&
-	// 		letterDetails?.reference_number
-	// 	) {
-	// 		const category =
-	// 			letterDetails?.current_state === "Draft" ? "draft" : "outbox";
-	// 		router.push(`/letters/${category}/${letterDetails?.reference_number}`);
-	// 	}
-	// }, [status, letterDetails]);
+	const onSubmit = () => {
+		mutate();
+	};
 
 	return (
 		<section className="flex items-center justify-between w-full sticky top-0">
 			<div className="flex gap-2">
 				<h1 className="page-title">አዲስ ደብዳቤ </h1>
 			</div>
-			{/* <div className="flex items-center gap-3">
-				{letterDetails?.letter_type === "incoming" ? null : <PrintPreviewButton />}
-				<Button
-					className="mr-0 RECIPIENTborder-gray-300 rounded-md"
-					variant="outline"
-					onClick={dispatchCreateLetter}
-				>
-					ረቂቁን ያስቀምጡ
-				</Button>
-				{letterDetails?.letter_type === "incoming" ? (
-					<SubmitLetterForm action="create_and_publish" />
-				) : (
-					<SubmitLetterForm action="create_and_submit" />
-				)}
-			</div> */}
+			<Button
+				className="mr-0 RECIPIENTborder-gray-300 rounded-md"
+				variant="outline"
+				onClick={onSubmit}
+			>
+				ረቂቁን ያስቀምጡ
+			</Button>
 		</section>
 	);
 }
