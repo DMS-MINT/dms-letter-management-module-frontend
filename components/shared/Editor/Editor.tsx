e7ba0e278ec5bb6d3d4e42c1555c80ea44da8002
Yab1 <yabilisanu@gmail.com>
2024-07-26 20:30:57 +0300

    feat(editor): add character count and limit to Editor component
    - Integrated tiptap editor with character count extension and character limit configuration.
    - Added useEditor hook for editor initialization and content management.
    - Connected editor state with Redux store for handling content updates.
    - Implemented character count percentage calculation to monitor usage.
    - Applied styling to editor content for consistent appearance.

---
+++ components/shared/Editor/Editor.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { EditorContent, useEditor } from "@tiptap/react";
+import CharacterCount from "@tiptap/extension-character-count";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import {
+	handleContentChange,
+	selectNewLetter,
+} from "@/lib/features/letterSlice";
+
+export default function Editor() {
+	const { content } = useAppSelector(selectNewLetter);
+	const dispatch = useAppDispatch();
+	const limit = 1000;
+
+	const editorEditable = useEditor({
+		editable: true,
+		content: content,
+		extensions: [
+			Document,
+			Paragraph,
+			Text,
+			CharacterCount.configure({
+				limit,
+			}),
+		],
+		immediatelyRender: false,
+		editorProps: {
+			attributes: {
+				class:
+					"max-w-[210mm] min-h-[20mm] max-h-[322px] mx-auto bg-white border border-gray-300",
+			},
+		},
+		onUpdate: ({ editor }) => dispatch(handleContentChange(editor.getHTML())),
+	});
+
+	const percentage = editorEditable
+		? Math.round(
+				(100 / limit) * editorEditable.storage.characterCount.characters()
+		  )
+		: 0;
+
+	return (
+		<section>
+			<EditorContent
+				style={{ whiteSpace: "pre-line" }}
+				editor={editorEditable}
+				className="editor"
+			/>
+		</section>
+	);
+}
