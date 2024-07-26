2eb0fae7bc1b4b9cd352e2d86b644cb9c503de8e
Yab1 <yabilisanu@gmail.com>
2024-07-26 20:36:01 +0300

    feat(compose): add letter prop to OutgoingLetterTemplate
    Updated the `Compose` component to pass a `letter` prop to the `OutgoingLetterTemplate`. This enhancement involves initializing `letter` with an empty `LetterDetailType` object and updating the `OutgoingLetterTemplate` to accept this prop. This change prepares the component for future integration of dynamic letter details.
    
    The `LetterDetailType` import has been added for type-checking and ensuring the correct data structure is used.

--- app/(root)/(letter management module)/letters/compose/page.tsx
+++ app/(root)/(letter management module)/letters/compose/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { Drawer, Subheader } from "@/components/layouts";
 import {
 	ComposeControlPanel,
 	LetterComposeDrawer,
 } from "@/components/letter_module";
 import { OutgoingLetterTemplate } from "@/components/letter_module/templates";
+import { LetterDetailType } from "@/types/letter_module";
 
 export default function Compose() {
+	const letter = {} as LetterDetailType;
 	return (
 		<>
 			<Subheader>
@@ -16,8 +20,7 @@ export default function Compose() {
 					<LetterComposeDrawer />
 				</Drawer>
 				<main className="mb-0 flex-1 flex flex-col bg-gray-100">
-					<div className="h-10 sticky top-0 bg-gray-100"></div>
-					{true ? <OutgoingLetterTemplate /> : null}
+					{true ? <OutgoingLetterTemplate letter={letter} /> : null}
 				</main>
 			</section>
 		</>
