0062893ba9e91f282922754d9f15bb2536ce7e69
Yab1 <yabilisanu@gmail.com>
2024-08-05 22:04:55 +0300

    feat(ui): extend UI store with letter editing and read-only states
    - added states for tracking whether a letter is being edited or is read-only.
    - introduced actions to update these new states in the UI store.
    - updated the zustand store to handle new UI state management.

--- stores/uiStore.ts
+++ stores/uiStore.ts
@@ -2,16 +2,25 @@ import { create } from "zustand";
 
 export type UIStoreType = {
 	isDrawerOpen: boolean;
+	isLetterEdited: boolean;
+	isLetterReadOnly: boolean;
 };
 
 export type Actions = {
 	toggleDrawerVisibility: () => void;
+	setLetterEdited: (_edited: boolean) => void;
+	setLetterReadOnly: (_readOnly: boolean) => void;
 };
 
 export const useUiStore = create<UIStoreType & Actions>()((set) => ({
 	isDrawerOpen: true,
+	isLetterEdited: false,
+	isLetterReadOnly: false,
 	toggleDrawerVisibility: () =>
 		set((state) => ({
 			isDrawerOpen: !state.isDrawerOpen,
 		})),
+
+	setLetterEdited: (edited) => set({ isLetterEdited: edited }),
+	setLetterReadOnly: (readOnly) => set({ isLetterReadOnly: readOnly }),
 }));
