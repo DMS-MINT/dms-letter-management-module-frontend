8c60aa80ac7ff16ddabd724096255af698ab37c4
Yab1 <yabilisanu@gmail.com>
2024-08-05 21:46:05 +0300

    feat(letter-compose): add letter type and language selection to compose drawer
    added functionality to select letter type and language in the letter compose drawer.
    integrated select components to allow users to choose the type of letter and language,
    with options being dynamically generated from `LanguageEnum`.
    
    BREAKING CHANGE: the letter compose drawer now includes language selection and type options
    with updated component structure.

--- components/drawers/LetterComposeDrawer.tsx
+++ components/drawers/LetterComposeDrawer.tsx
@@ -1,17 +1,78 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
-import { Mail, Paperclip } from "lucide-react";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
+import { useLetterStore } from "@/stores";
+import { LanguageEnum } from "@/types/shared";
+import { Languages, Mail, Paperclip } from "lucide-react";
 
 export default function LetterComposeDrawer() {
+	const { updateLetterField, letter_type, language } = useLetterStore(
+		(state) => ({
+			letter_type: state.letter_type,
+			language: state.language,
+			updateLetterField: state.updateLetterField,
+		})
+	);
+
+	const handleLetterTypeChange = (type: string): void => {
+		updateLetterField("letter_type", type);
+	};
+
+	const handleLanguageChange = (type: LanguageEnum): void => {
+		updateLetterField("language", type);
+	};
+
 	return (
 		<section className="flex flex-col gap-10">
 			<div className="flex flex-col gap-2">
 				<div className="flex items-center gap-2">
 					<Mail size={20} className="text-gray-600" />
-					<p className="text-gray-600">የደብዳቤ አይነት</p>
+					<p className="text-gray-600">የደብዳቤ ዓይነት</p>
 				</div>
-				<Badge className="mb-2 h-10 rounded-sm bg-gray-200 text-base font-normal text-gray-900"></Badge>
+				<Select value={letter_type} onValueChange={handleLetterTypeChange}>
+					<SelectTrigger>
+						<SelectValue placeholder="የደብዳቤ ዓይነት ይምረጡ" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							<SelectItem value="internal" className="pr-2">
+								የውስጥ ደብዳቤ
+							</SelectItem>
+							<SelectItem value="outgoing" className="pr-2">
+								ወደ ውጪ የሚላክ
+							</SelectItem>
+							<SelectItem value="incoming" className="pr-2">
+								ከውጭ የተላከ
+							</SelectItem>
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+				<div className="flex items-center gap-2">
+					<Languages size={20} className="text-gray-600" />
+					<p className="text-gray-600">ቋንቋ ይምረጡ</p>
+				</div>
+				<Select value={language} onValueChange={handleLanguageChange}>
+					<SelectTrigger>
+						<SelectValue placeholder="ቋንቋ ይምረጡ" />
+					</SelectTrigger>
+					<SelectContent>
+						<SelectGroup>
+							{Object.entries(LanguageEnum).map(([key, value]) => (
+								<SelectItem key={key} value={value} className="pr-2">
+									{key}
+								</SelectItem>
+							))}
+						</SelectGroup>
+					</SelectContent>
+				</Select>
 				<div className="flex items-center gap-2">
 					<Paperclip size={20} className="text-gray-600" />
 					<p className="text-gray-600">የተያያዙ ፋይሎች</p>
