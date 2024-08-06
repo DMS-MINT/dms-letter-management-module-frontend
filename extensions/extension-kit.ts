745827f5289be9b5f784d4af062a3bd7fd91b5c2
Yab1 <yabilisanu@gmail.com>
2024-08-06 12:16:28 +0300

    feat(editor): add new extensions to the extension kit
    - added BlockquoteFigure, CharacterCount, Color, Document, Dropcursor, FontSize,
      Heading, Highlight, HorizontalRule, Link, TaskItem, TaskList, TextAlign, TextStyle,
      Underline to the ExtensionKit.
    - configured each extension with appropriate options.
    - removed some default configurations from StarterKit.
    
    BREAKING CHANGE: the editor now includes a more extensive set of extensions

--- extensions/extension-kit.ts
+++ extensions/extension-kit.ts
@@ -1,10 +1,69 @@
 "use client";
 
-import { Placeholder, StarterKit } from ".";
+import {
+	BlockquoteFigure,
+	CharacterCount,
+	Color,
+	Document,
+	Dropcursor,
+	FontSize,
+	Heading,
+	Highlight,
+	HorizontalRule,
+	Link,
+	Placeholder,
+	StarterKit,
+	TaskItem,
+	TaskList,
+	TextAlign,
+	TextStyle,
+	Underline,
+} from ".";
 
 export const ExtensionKit = () => [
-	StarterKit,
+	Document,
+	TaskList,
+	TaskItem.configure({
+		nested: true,
+	}),
+	// Selection,
+	Heading.configure({
+		levels: [1, 2, 3, 4, 5, 6],
+	}),
+	HorizontalRule,
+	StarterKit.configure({
+		document: false,
+		dropcursor: false,
+		heading: false,
+		horizontalRule: false,
+		blockquote: false,
+		history: false,
+		codeBlock: false,
+	}),
+	TextStyle,
+	FontSize,
+	Color,
+	Link.configure({
+		openOnClick: false,
+	}),
+	Highlight.configure({ multicolor: true }),
+	Underline,
+	CharacterCount.configure({ limit: 50000 }),
+	TextAlign.extend({
+		addKeyboardShortcuts() {
+			return {};
+		},
+	}).configure({
+		types: ["heading", "paragraph"],
+	}),
 	Placeholder.configure({
-		placeholder: "ደብዳቤዎን እዚህ ይፃፉ...",
+		includeChildren: true,
+		showOnlyCurrent: false,
+		placeholder: () => "ደብዳቤዎን እዚህ ይፃፉ...",
+	}),
+	BlockquoteFigure,
+	Dropcursor.configure({
+		width: 2,
+		class: "ProseMirror-dropcursor border-black",
 	}),
 ];
