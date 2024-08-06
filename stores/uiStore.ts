fa05a8bbfacb3cd971814046e42c77522edd5400
Yab1 <yabilisanu@gmail.com>
2024-08-06 23:03:45 +0300

    refactor(ui): rename actions type for consistency
    - renamed Actions type to UIActions to improve naming consistency.
    - no functional changes were made to the store logic or the state structure.

--- stores/uiStore.ts
+++ stores/uiStore.ts
@@ -6,13 +6,13 @@ export type UIStoreType = {
 	isLetterReadOnly: boolean;
 };
 
-export type Actions = {
+export type UIActions = {
 	toggleDrawerVisibility: () => void;
 	setLetterEdited: (_edited: boolean) => void;
 	setLetterReadOnly: (_readOnly: boolean) => void;
 };
 
-export const useUiStore = create<UIStoreType & Actions>()((set) => ({
+export const useUiStore = create<UIStoreType & UIActions>()((set) => ({
 	isDrawerOpen: true,
 	isLetterEdited: false,
 	isLetterReadOnly: false,
