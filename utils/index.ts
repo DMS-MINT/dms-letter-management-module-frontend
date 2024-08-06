7ebf9baafa8dfe0fcebc1460b59858291b598c15
Yab1 <yabilisanu@gmail.com>
2024-08-06 12:17:05 +0300

    fix(editor): correct text selection detection
    - improved the logic to check for empty text blocks when double-clicking in empty paragraphs.
    - ensured the function properly handles cases where the editor is not editable.

--- utils/index.ts
+++ utils/index.ts
@@ -2,3 +2,4 @@ export { default as canSubmitLetter } from "./canSubmitLetter";
 export { default as convertToEthiopianDate } from "./convertToEthiopianDate";
 export { default as generateUserPermissions } from "./generateUserPermissions";
 export { default as getParticipantInfo } from "./getParticipantInfo";
+export { default as isTextSelected } from "./isTextSelected";
