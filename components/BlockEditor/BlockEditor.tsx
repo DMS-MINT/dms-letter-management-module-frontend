09bc39116a30a37c69b5af80773b486727002b90
Yab1 <yabilisanu@gmail.com>
2024-08-07 13:56:21 +0300

    feat(block-editor): integrate path-based editor reset and read-only state
    - added usePathname hook to manage path-based logic.
    - integrated useUiStore to handle read-only state of editor.
    - implemented editor reset on path change to "/letters/compose".
    - updated BlockEditor component to conditionally clear content and set read-only state.
    
    BREAKING CHANGE: BlockEditor now depends on the pathname and UI store for read-only logic.

--- components/BlockEditor/BlockEditor.tsx
+++ components/BlockEditor/BlockEditor.tsx
@@ -1,14 +1,33 @@
 "use client";
 
 import useBlockEditor from "@/hooks/useBlockEditor";
+import { useUiStore } from "@/stores";
 import "@/styles/index.css";
 import { EditorContent } from "@tiptap/react";
+import { usePathname } from "next/navigation";
+import { useCallback, useEffect } from "react";
 import { TextMenu } from "./menus/TextMenu";
 
 export default function BlockEditor() {
-	const { editor } = useBlockEditor();
+	const pathname = usePathname();
+	const { isLetterReadOnly, setLetterReadOnly } = useUiStore((state) => ({
+		isLetterReadOnly: state.isLetterReadOnly,
+		setLetterReadOnly: state.setLetterReadOnly,
+	}));
+	const { editor } = useBlockEditor({ editable: !isLetterReadOnly });
 
-	if (!editor) return;
+	const resetEditorContent = useCallback(() => {
+		if (editor && pathname === "/letters/compose") {
+			editor.commands.clearContent(true);
+			setLetterReadOnly(false);
+		}
+	}, [editor, pathname, setLetterReadOnly]);
+
+	useEffect(() => {
+		resetEditorContent();
+	}, [resetEditorContent]);
+
+	if (!editor) return null;
 
 	return (
 		<section className="mb-10 mt-5 w-full">
