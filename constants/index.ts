82244897014d3fd7ea708727ce6e7dd0f4541f18
Yab1 <yabilisanu@gmail.com>
2024-08-13 19:51:23 +0300

    feat(constants): add new documentation and video links
    - added lms walkthrough video link to LINKS.
    - introduced DOCS object with a user manual link.

--- constants/index.ts
+++ constants/index.ts
@@ -9,4 +9,10 @@ export const IMAGES = Object.freeze({
 export const LINKS = Object.freeze({
 	google_authenticator:
 		"https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&pcampaignid=web_share",
+	lms_walkthrough_video:
+		"https://drive.google.com/file/d/1Lj23UNXBDmNSfLuo8UtnacY5U4JjShh7/preview",
+});
+
+export const DOCS = Object.freeze({
+	user_manual: "/docs/user-manual.pdf",
 });
