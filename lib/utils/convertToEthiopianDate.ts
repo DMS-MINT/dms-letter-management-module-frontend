2fa005f2402f9833e0e7f0a8c10cc8696cea673a
Yab1 <yabilisanu@gmail.com>
2024-08-13 19:32:43 +0300

    refactor(utils): move utility functions to lib/utils
    - moved utility functions from `/utility` to `/lib/utils`.
    - updated import paths throughout the project to reflect new locations.
    - cleaned up obsolete utility directory.

---
+++ lib/utils/convertToEthiopianDate.ts
@@ -0,0 +1,38 @@
+import { monthTranslations } from "@/types/letter_module";
+const ethiopianDate = require("ethiopian-date");
+
+export function convertToEthiopianDate(dateString: string): string {
+	const date = new Date(dateString);
+
+	const [ethYear, ethMonth, ethDay] = ethiopianDate.toEthiopian(
+		date.getFullYear(),
+		date.getMonth() + 1,
+		date.getDate()
+	);
+
+	return `${monthTranslations[ethMonth]} ${ethDay} ${ethYear}`;
+}
+
+export function convertToEthiopianDateAndTime(dateString: string): {
+	time: string;
+	date: string;
+} {
+	const date = new Date(dateString);
+
+	const [ethYear, ethMonth, ethDay] = ethiopianDate.toEthiopian(
+		date.getFullYear(),
+		date.getMonth() + 1,
+		date.getDate()
+	);
+
+	// Formatting the time component
+	const hours = date.getHours().toString().padStart(2, "0");
+	const minutes = date.getMinutes().toString().padStart(2, "0");
+	// const seconds = date.getSeconds().toString().padStart(2, "0");
+
+	// Return an object with separate parts
+	return {
+		time: `${hours}:${minutes}`,
+		date: `${monthTranslations[ethMonth]} ${ethDay}, ${ethYear}`,
+	};
+}
