a4f6a78a11ade265601e02587723ef546d84fa54
Yab1 <yabilisanu@gmail.com>
2024-08-15 00:17:55 +0300

    feat(websocket): add custom React hook for WebSocket management
    - implemented `useWebsocket` hook for managing WebSocket connections
    - included automatic reconnection with configurable interval
    - added handling for WebSocket open, message, close, and error events
    
    BREAKING CHANGE: this hook introduces a new way to manage WebSocket connections

--- hooks/index.ts
+++ hooks/index.ts
@@ -2,5 +2,6 @@ export * from "./useOTP";
 export * from "./useParticipantSelector";
 export { default as useParticipantSelector } from "./useParticipantSelector";
 export { default as useToastMutation } from "./useToastMutation";
+export { default as useWebsocket } from "./useWebsocket";
 export * from "./useWorkflowDispatcher";
 export { default as useWorkflowDispatcher } from "./useWorkflowDispatcher";
